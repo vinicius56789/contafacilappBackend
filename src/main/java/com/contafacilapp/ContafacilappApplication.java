c5d9e74056785892ee9efefbfd3821131b31055c
Vinícius Silveira de Melo <viniciusv638@gmail.com>
2022-08-16 00:44:57 -0300

    arcabouço

---
+++ src/main/java/com/contafacilapp/ContafacilappApplication.java
@@ -0,0 +1,13 @@
+package com.contafacilapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContafacilappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContafacilappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contafacilapp/ContafacilappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contafacilapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContafacilappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
